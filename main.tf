46ccfd1b68c5576e903bca1fcee4f3a6e9f9d509
Narendra Ingale <narendra.ingale@appzen.com>
2020-08-17 11:05:10 +0530

    Added code for acm

---
+++ main.tf
@@ -0,0 +1,23 @@
+resource "aws_acm_certificate" "cert" {
+	domain_name       = "${var.acm_cert_domain}"
+  validation_method = "DNS"
+
+	tags = {
+		Environment = "${var.environment}"
+  }
+
+}
+
+data "aws_route53_zone" "route53" {
+  name = "${var.dns_parent_zone_name}."
+}
+
+resource "aws_route53_record" "validation_record" {
+	depends_on = ["aws_acm_certificate.cert"]
+	count    = "${length(aws_acm_certificate.cert.domain_validation_options)}"
+	name     = "${lookup(aws_acm_certificate.cert.domain_validation_options[count.index], "resource_record_name")}"
+	type     = "${lookup(aws_acm_certificate.cert.domain_validation_options[count.index], "resource_record_type")}"
+	records  = ["${lookup(aws_acm_certificate.cert.domain_validation_options[count.index], "resource_record_value")}"]
+	zone_id  = "${data.aws_route53_zone.route53.zone_id}"
+	ttl      = 60
+}
---
+++ variables.tf
@@ -0,0 +1,12 @@
+
+variable "dns_parent_zone_name" {
+
+}
+
+variable "acm_cert_domain" {
+
+}
+
+variable "environment" {
+
+}
